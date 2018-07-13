10574b795c0b56a2ebbcff2244eeb8c7d56db023
Caroline Berg <carolineeberg@gmail.com>
2018-07-13 21:45:03 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
 def position_taken?(board, index)
-  if board[index] == ("X" || "O")
+  if board[index] == "X" || board[index] == "O"
   else board[index] == !(" " || "")
   end
 end
