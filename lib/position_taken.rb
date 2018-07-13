b34f4c5a9baf82ab0f6db4fd50747dadcfe8dceb
Caroline Berg <carolineeberg@gmail.com>
2018-07-13 21:16:05 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 board = Array.new(9, " ")
 def position_taken?(board, index)
-  if board[index] == "X"
-  elsif board[index] == "O"
+  if board[index] != "X"
+  elsif board[index] != "O"
   else board[index] == !(" " || "")
   end
 end
